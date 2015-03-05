c8a09ddcd9a094556c2b56b3315067594e5373b0
Tyler Renelle <tylerrenelle@gmail.com>
2015-03-05 09:27:45 -0700

    full error reporting

--- app.js
+++ app.js
@@ -33,29 +33,14 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
+// error handler
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.send({
     message: err.message,
-    error: {}
+    error: err
   });
+  console.error(err);
 });
-
 
 module.exports = app;
