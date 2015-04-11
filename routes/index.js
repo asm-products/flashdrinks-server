0f561297d87ebfd3e8bc1ec46e21e57fda9018eb
Tyler Renelle <tylerrenelle@gmail.com>
2015-04-11 15:52:19 -0600

    feat(push): WIP push-notifications

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var nconf = require("nconf");
 var multipart = require('connect-multiparty');
 var multipartMiddleware = multipart();
+var AWS = require('s3-uploader/node_modules/aws-sdk');
 
 router.get('/', function(req, res, next) {
   res.send(200);
@@ -54,7 +55,32 @@ router.post('/s3-upload', multipartMiddleware, function(req, res, next){
     if (err) return next(err);
     res.json(images);
   });
-})
+});
 
+// ------ SNS ------
+// See http://t.yc.sg/post/102663623041/amazon-sns-with-ionic-framework-part-1-android & http://stackoverflow.com/questions/21609121/anyone-using-node-js-with-amazon-sns-and-apple-push-notifications
+AWS.config.update({accessKeyId: nconf.get('aws:accessKeyId'), secretAccessKey: nconf.get('aws:secretAccessKey')});
+AWS.config.update({region: nconf.get('aws:region')});
+var sns = new AWS.SNS();
+router.post('/subscribe-push', function(req, res, next){
+  if (!req.body.token) return res.json(400, {"status": "error", "message": "Please provide a token."});
+  if (!req.body.platform) return res.json(400, {"status": "error", "message": "Please provide a platform."});
+  var token = req.body.token,
+    platform = req.body.platform,
+    arn = (platform=='GCM' /*|| APNS*/) ? nconf.get("aws:sns:"+platform) : false;
+  if (!arn) return res.json(400, {"status": "error", "message": "Please provide a valid platform."});
+  sns.createPlatformEndpoint({PlatformApplicationArn:platformApplicationArn, Token:token}, function(err, EndPointResult) {
+    if (err) return res.json(400, {status: "error", "message": err.toString()});
+    return res.json(200, {status: "ok"});
+    //var client_arn = EndPointResult["EndpointArn"];
+    //sns.publish({TargetArn: client_arn, Message: 'Test', Subject: 'Stuff'}, function(err,data){
+    //  if (err) {
+    //    console.log("Error sending a message "+err);
+    //  } else {
+    //    console.log("Sent message: "+data.MessageId);
+    //  }
+    //});
+  });
+})
 
 module.exports = router;
